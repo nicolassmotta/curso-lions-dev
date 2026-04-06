a1281ff3851d3f671562add4cf0aa6c4ebf5ceed
Nicolas Cardoso Motta <nicolas.c.motta@hotmail.com>
2026-04-06 09:09:37 -0300

    feat: semicoluns removed

--- Modulo06/exercicio_slide/removerContato.js
+++ Modulo06/exercicio_slide/removerContato.js
@@ -3,7 +3,7 @@ function removerContato(contatos, id) {
     const index = contatos.findIndex(contato => contato.id === id)
     
     if (index !== -1) {
-        contatos.splice(index, 1);
+        contatos.splice(index, 1)
     }
 }
 
