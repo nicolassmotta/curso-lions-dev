07730efe2df089abefae42a85ab434f9e7f7cb7a
Nicolas Motta <nicolas.c.motta@hotmail.com>
2026-06-09 10:57:31 -0300

    feat: exercise of petshop solved

--- modulo08/exercicios_resolvidos/petshop/src/db.js
+++ modulo08/exercicios_resolvidos/petshop/src/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({path: "../.env"});
 const MONGO_URI = process.env.MONGO_URI;
 
 async function conectarDB() {
--- modulo08/exercicios_resolvidos/petshop/src/server.js
+++ modulo08/exercicios_resolvidos/petshop/src/server.js
@@ -1,9 +1,9 @@
 import express from "express";
+import dotenv from "dotenv";
 import Agendamento from "./models/agendamento.js";
 import conectarDB from "./db.js";
-import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config({path: "../.env"});
 const app = express();
 const PORT = process.env.PORT;
 conectarDB();
@@ -88,6 +88,63 @@ app.post("/agendamentos", async (req, res) => {
   }
 });
 
+app.get(("/agendamentos"), async (req, res) => {
+  try {
+    const todosAgendamentos = await Agendamento.find();
+    res.status(200).json({mensagem: "Todos os agendamentos foram listados com sucesso!", agendamentos: todosAgendamentos});
+  } catch (erro) {
+    res.status(400).json({mensagem: `Erro ao listar os agendamentos: ${erro.message}`})
+  }
+});
+
+app.get(("/agendamentos/busca"), async (req, res) => {
+  try {
+    const nome = req.query.nome;
+    const agendamentos = await Agendamento.find({nomePet: 
+      { $regex: nome, $options: "i"}}); 
+    // regex busca por partes do texto
+    // options ignora maiusculas e minusculas
+    res.status(200).json({mensagem: "Busca efetuada com sucesso!", 
+      agendamentos: agendamentos});
+  } catch (erro) {
+    res.status(400).json({mensagem: `Erro: ${erro.message}`})
+  }
+});
+
+app.patch(("/agendamentos/:id"), async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { status } = req.body;
+    const agendamentoAtualizado = await Agendamento.findByIdAndUpdate(id, 
+      {status: status}, {runValidators: true, new: true});
+
+    if (!agendamentoAtualizado) {
+      return res.status(404).json({mensagem: "Agendamento não encontrado!"});
+    }
+
+    res.status(200).json({mensagem: "Agendamento atualizado com sucesso!", 
+      agendamentoAtualizado: agendamentoAtualizado});
+  } catch (erro) {
+    res.status(500).json({mensagem: `Erro do servidor: ${erro.message}`});
+  }
+});
+
+app.delete(("/agendamentos/:id"), async (req, res) => {
+  try {
+    const id = req.params.id;
+    const agendamentoDeletado = await Agendamento.findByIdAndDelete(id);
+
+    if (!agendamentoDeletado) {
+      return res.status(404).json({mensagem: "Agendamento não encontrado!"});
+    }
+
+    res.status(200).json({mensagem: "Agendamento deletado com sucesso!", agendamentoDeletado: agendamentoDeletado});
+  } catch (erro) {
+    res.status(500).json({mensagem: `Erro do servidor: ${erro.message}`});
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Conectado com a porta ${PORT} com sucesso!`);
 });
+
